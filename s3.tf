d0abe67f9470f3157d49827e2c6db02588c6d3ed
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-05-05 15:23:31 -0500

    added s3

---
+++ s3.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "${var.domain-name}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version":"2008-10-17",
+    "Statement":[{
+    "Sid":"AllowPublicRead",
+    "Effect":"Allow",
+    "Principal": {"AWS": "*"},
+    "Action":["s3:GetObject"],
+    "Resource":["arn:aws:s3:::${var.domain-name}/*"]
+    }]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
